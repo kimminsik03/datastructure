93350a8904efff5c50afec0f0411b97afc0e3b26
minsik <kim@66694.gmail.com>
2025-03-24 23:18:19 +0900

    array_4

--- Week3_Review.py
+++ Week3_Review.py
@@ -1,25 +1,10 @@
-def move_zeros(list):
-    zero_index =0
-    for index , value in enumerate(list):
-            if value != 0: #0이 아닌 값을 왼쪽으로 옮기기 위해
-                list[zero_index] = value     #0이 아닌 값을 실제로 맨 왼쪽부터 옮김
-                if zero_index != index:     #둘이 같지 않다는 것은 위에서 index에 값을 복사했다는 뜻
-                    list[index] =0          #위에서 index에 value값을 복사하였으니 원래값(index)은 0으로 바뀜
-                zero_index+=1
-    return(list)
+list1 =[1,2,3,4,5]
+list2 =["하나","둘","셋","넷","다섯"]
 
-list =[1,0,7,12,0,4]
-move_zeros(list)
-print(list)
+result = list(zip(list1,list2))
+# zip은 2개의 리스틀 튜플형태로 모아줌
+# zip의 결과를 볼려면 list형태로 감싸줘야 함
+
+print(result)
 
 
-# list = [1,2,3,4,5]
-#
-# for i in range (len(list)):
-#     print(i,list[i])
-
-#enumerate 함수는 값과 인덱스를 함께 반환
-#for i in enumerate(list)
-#=> 여기에서 i는 튜플 형태로 받음
-#ex) (0,1) (1,10)
-#=> 이걸 언팩킹하기 위해서 반복문에 2개의 변수를 사용
