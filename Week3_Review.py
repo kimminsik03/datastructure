4c0ede256eaa9cdbc063f74d9526e8d259c41c24
minsik <kim@66694.gmail.com>
2025-03-24 23:20:26 +0900

    array_7

--- Week3_Review.py
+++ Week3_Review.py
@@ -1,21 +1,22 @@
-#중복값을 출력하기
-
-def sameValue (city):
-    list = list() #중복값을 출력할 리스트
-    s = set()
-    for city in city: #시티 배열에서 도시를 하나씩 빼옴
-        l1 = len(s)  #길이를 구함
-        s.add(city)  #값을 넣어줌
-        l2 = len(s)  #길이를 구함 만약 중복 값이 들어오지 않으면 길이는 변하지 않음
-        if l1 == l2: #따라서 두 변수의 길이가 같다는 것은 중복값이 들어왔다는 걸 의미
-            list.append(city) # 중복값 배열에 넣기
+#방법 1 리스트 축약으로 교집합을 확인
+# def same(list1,list2):
+#     list3 = [v for v in list1 if v in list2 ]
+#     # 리스트 축약
+#     #풀어보면  list1을 돌면서 안에 있는 값이 v를 꺼내서 list2에 있는 v값이 같으면 list3에 v(맨 앞에)를 추가(append)
+#     return(list3)
 
 
-city = ["인천","서울","김포","수원","서울","부산"]
-city.append("김포") #set은 add 속성을 통하여 값을 추가할 수 있음 다만 중복 값은 무시됨
-city.append("대전")
-print(city)
+#방법 2 set함수 안에는 intersection() 즉 교집합을 구하는 함수가 있으므로 이를 활용함
+def same(list1,list2):
+   set1 = set(list1)
+   set2 = set(list2)
+   return list(set1.intersection(set2)) # set1 & set2
 
+
+list1  = [1,5,6,11,25,13]
+list2  = [2,5,7,12,11,25]
+list3 = same(list1,list2)
+print(list3)
 
 
 
